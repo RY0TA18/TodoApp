bd43f3e32ff41f145048e62bf42d49c454abf7ba
mizushima <mizushima@mizushimanoMacBook-Pro.local>
2024-09-08 17:55:33 +0900

    routerをファイル分割

---
+++ lib/router.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:my_todo/main.dart';
+
+final GoRouter router = GoRouter(
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (BuildContext context, GoRouterState state) => const TodoListPage(),
+    ),
+    GoRoute(
+      path: '/addTodoPage',
+      builder: (BuildContext context, GoRouterState state) => const AddTodoPage(),
+    ),
+  ],
+);
