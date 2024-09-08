9d657ffd9df87743ded77505f033f5e8a6b694e5
mizushima <mizushima@mizushimanoMacBook-Pro.local>
2024-09-08 18:34:33 +0900

    追加を押下するとtasks配列にタスクを追加

--- lib/pages/add_todo_page.dart
+++ lib/pages/add_todo_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-// import 'package:my_todo/todo_list.dart';
+import 'package:my_todo/todo_list.dart';
 
 class AddTodoPage extends StatefulWidget {
   const AddTodoPage({super.key});
@@ -38,6 +38,7 @@ class _AddTodoPageState extends State<AddTodoPage> {
                 ElevatedButton(
                   onPressed: () {
                     GoRouter.of(context).go('/');
+                    tasks.add(todoText); // Step 2: 入力値をリストに追加
                     print(todoText); // Step 3: 入力値を表示
                   },
                   child: const Text('追加'),
