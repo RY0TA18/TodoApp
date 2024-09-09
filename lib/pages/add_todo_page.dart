6a8d5f6a24bb1c95e3f426bbf68d2e908be4c3dc
mizushima <mizushima@mizushimanoMacBook-Pro.local>
2024-09-09 13:07:12 +0900

    問題の部分の修正

--- lib/pages/add_todo_page.dart
+++ lib/pages/add_todo_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:my_todo/todo_list.dart';
@@ -37,11 +38,13 @@ class _AddTodoPageState extends State<AddTodoPage> {
             Row(
               children: [
                 ElevatedButton(
-                  onPressed: () async {
+                  onPressed: () {
                     if (todoText.isNotEmpty) {
-                      await todoService.addTask(todoText);
+                      todoService.addTask(todoText);
                       GoRouter.of(context).go('/');
-                      print(todoText); // 入力値を表示
+                      if (kDebugMode) {
+                        print(todoText);
+                      } // 入力値を表示
                     }
                   },
                   child: const Text('追加'),
