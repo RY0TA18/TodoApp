e34bb7fce14007980dd16a78805869ee848549cd
Ryota <ryota1625@gmail.com>
2024-09-10 22:38:05 +0900

    DoneTodoPageをルートに追加

--- lib/pages/done_todo_page.dart
+++ lib/pages/done_todo_page.dart
@@ -1,26 +1,21 @@
-// import 'package:flutter/material.dart';
-// import 'package:my_todo/components/bottom_navigationbar.dart';
-// import 'package:my_todo/components/tasktile.dart';
-// import 'package:my_todo/todo_list.dart';
+import 'package:flutter/material.dart';
+import 'package:my_todo/components/bottom_navigationbar.dart';
 
-// class DoneTodoPage extends StatelessWidget {
-//   const DoneTodoPage({super.key});
+class DoneTodoPage extends StatelessWidget {
+  const DoneTodoPage({super.key});
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Text('Done Todo List'),
-//       ),
-//       body: ListView.builder(
-//         itemCount: doneTasks.length,
-//         itemBuilder: (BuildContext context, int index) {
-//           return TaskTile(tasks: doneTasks, index: index);
-//         },
-//       ),
-//       bottomNavigationBar: const CustomBottomNavigationBar(
-//         currentIndex: 1,
-//       ),
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Done Todo List'),
+      ),
+      body: const Center(
+        child: Text('Done Todo List'),
+      ),
+      bottomNavigationBar: const CustomBottomNavigationBar(
+        currentIndex: 1,
+      ),
+    );
+  }
+}
